a60723a9ba644ce9c216ee5d0d31f345e3aafe54
Emily Janzer <emily.janzer@gmail.com>
2014-03-18 15:53:48 -0700

    stubbing methods for image normalization

--- normalize.py
+++ normalize.py
@@ -1,1 +1,21 @@
 from PIL import Image
+
+THRESHOLD_OFFSET = 20
+
+def open_image(path):
+    pass
+
+def check_image(im):
+    pass
+
+def resize_image(im):
+    pass
+
+def threshold_image(im):
+    pass
+
+def sample_corners(im):
+    pass
+
+def crop_image(im):
+    pass
