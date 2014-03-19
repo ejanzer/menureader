e75ab3e665d627c5d7096ad42ad6650a61bc54ab
Emily Janzer <emily.janzer@gmail.com>
2014-03-18 17:49:18 -0700

    finished seeding db with test users, reviews and tags

--- model/seed.py
+++ model/seed.py
@@ -1,10 +1,18 @@
 import sqlalchemy
+import os
+import datetime
 
 import csv
 from base import Base, engine, db_session
 from dict_entry import Dict_Entry
 from dish import Dish
 from food_word import Food_Word
+from user import User
+from review import Review 
+from dish_tag import Dish_Tag 
+from rest_dish import Rest_Dish 
+from restaurant import Restaurant
+from tag import Tag
 
 def seed_cedict():
     """Seed the Chinese/English dictionary with data from CEDICT."""
@@ -72,29 +80,86 @@ def seed_food_words():
 
 def seed_users():
     """Seed users with test users."""
-    pass
 
-def seed_reviews():
-    """Seed reviews with test data."""
-    pass
+    emilypass = os.environ.get("EMILYPASS")
+    emily = User(username='emily')
+    emily.set_password(emilypass)
 
-def seed_tags():
-    """Seed tags with test data."""
-    pass
+    nickpass = os.environ.get("NICKPASS")
+    nick = User(username="nick")
+    nick.set_password(nickpass)
+
+    db_session.add(emily)
+    db_session.add(nick)
+    db_session.commit()
 
 def seed_restaurants():
     """Seed restaurants with test data."""
-    pass
+    r1 = Restaurant(name="Chef Zhao's")
+    r2 = Restaurant(name="Fu Lam Mum")
+    r3 = Restaurant(name="Chef Chu's")
+    r4 = Restaurant(name="Queen House")
+    db_session.add(r1)
+    db_session.add(r2)
+    db_session.add(r3)
+    db_session.add(r4)
+    db_session.commit()
+
+def seed_reviews():
+    """Seed reviews with test data."""
+
+    ##### Add a review for Hot and Sour Soup at Fu Lam Mum from Emily. #####
+    add_review(1, 107, 2, 1, "too spicy!")
+
+    ##### Add a review for Kung Pao Chicken at Cafe Macs from Nick. #####
+    add_review(2, 6, 5, 2, "Very peanut\nMuch tasty\nWow")
+
+    ##### Add a review for Chongqing Chicken at Chef Zhao's from Emily. #####
+    add_review(1, 218, 1, 1, "nom so hard")
+
+
+
+def add_review(user_id, dish_id, rest_id, tag_id, text):
+    now = datetime.datetime.now()
+
+    rest_dish = Rest_Dish(dish_id=dish_id, rest_id=rest_id)
+    db_session.add(rest_dish)
+
+    dish_tag = Dish_Tag(dish_id=dish_id, tag_id=tag_id, rest_dish_id=rest_dish.id)
+    db_session.add(dish_tag)
+
+    review = Review(user_id=user_id, rest_dish_id=rest_dish.id, dish_id=dish_id, text=text, date=now)
+    db_session.add(review)
+
+    db_session.commit()
+
+
+def seed_tags():
+    """Seed tags with test data."""
+    tags = [
+        Tag(name="spicy"),
+        Tag(name="chicken"),
+        Tag(name="pork"),
+        Tag(name="beef"),
+        Tag(name="vegetarian"),
+        Tag(name="fish")
+    ]
+
+    for tag in tags:
+        db_session.add(tag)
+
+    db_session.commit()
 
 if __name__ == "__main__":
     # Uncomment and run as main to seed entire database.
     # Or, run in interactive mode and call the function for the table you want to seed.
     # seed_cedict()
     # seed_dishes()
-    seed_food_words()
+    # seed_food_words()
     # seed_users()
     # seed_reviews()
     # seed_tags()
     # seed_restaurants()
+    pass
 
 
