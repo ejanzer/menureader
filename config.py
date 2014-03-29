58aad58e208f7f9a976fcb16649f02cc30cde52e
Emily Janzer <emily.janzer@gmail.com>
2014-03-29 16:09:34 -0700

    environment variables

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 # Config file
 DB_URI = os.environ.get("DATABASE_URL", "sqlite:///menureader.db")
-SECRET_KEY = "shhhhitsasecret"
+SECRET_KEY = os.environ.get("SECRET_KEY", "shhhhhhhhh")
